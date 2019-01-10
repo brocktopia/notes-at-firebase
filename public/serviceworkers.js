59d4b4b258090ae9be03311321db95ca8ee60654
Brock Henderson <brocktopia@gmail.com>
2019-01-10 14:21:38 -0800

    Updated version

--- public/serviceworkers.js
+++ public/serviceworkers.js
@@ -1,4 +1,4 @@
-const version = 'v1.1.0';
+const version = 'v1.2.0';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ Vue.prototype.$firebase = firebase;
 
 Vue.prototype.$moment = moment;
 
-Vue.prototype.$version = '1.1.0';
+Vue.prototype.$version = '1.2.0';
 
 // Moved map config to external file to reduce chances of committing secret key
 Vue.use(VueGoogleMaps, {
